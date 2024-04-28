96cd056d346161df3c275960d061f573917e2de8
RLungWu <larry14789@gmail.com>
2024-04-29 00:29:45 +0800

    Worker Complete

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -34,7 +34,6 @@ type Coordinator struct {
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	log.Pringln("MakeCoordinator")
 
-
 	c := Coordinator{}
 	c.MapChannel = make(chan *Task, len(files))
 	c.ReduceChannel = make(chan *Task, nReduce)
@@ -103,6 +102,345 @@ func (c *Coordinator) genReduceTasks(){
 }
 
 
+//Periodically remove expired tasks and reset them into ready status
+func (c *Coordinator) peroidocallyRmExpTasks(){
+	for !c.Done(){
+		time.Sleep(time.second)
+		c.Lock.Lock()
+		if c.AssignPhase == MapPhase{
+			for i := 0; i < c.MapperNum; i++{
+				task := <- c.MapChannel
+				c.MapChannel <- task
+				if task.TaskStatus == Running &&  (time.Now().Sub(task.BeginTime) > TaskExpiredTime * time.Second){
+					task.Taskstatus = Ready
+					log.Pringf("Task with MapWorker id = %d is expired", task.MapWorkerId)
+				}
+			}
+		}else{
+			for i := 0; i < c.ReducerNum; i++{
+				task := c.ReduceChannel
+				c.ReduceChannel <- task
+				if task.TaskStatus == Running && (time.Now().Sub(task.BeginTime) > TaskExpiredTime * time.Second){
+					task.TaskStatus = Ready
+					log.Pringf("Task with ReduceWorker id = %d is expired", task.ReduceWorkerId)
+				}
+			}
+		}
+
+		c.Lock.Unlock()
+	}
+}
+
+func (c *Coordinaotr) AssignTask(args *TaskArgs, reply *TaskReply) error{
+	c.Lock.Lock()
+	
+	if c.AllMapDone && c.AllReduceDone{
+		reply.AllDone = tmp_reduce_file
+		c.Lock.Unlock()
+		return nil
+	}
+
+	if c.AssignPhase == MapPhase{
+		for i := 0; i < c.MapperNum; i++{
+			task := <- c.MapChannel
+			c.MapChannel <- task
+			
+			if task.TaskStatus == Ready{
+				task.MapWorkerId = c.MapWorkerId
+				c.MapWorkerId++
+				task.TaskStatus = Running
+				task.BeginTime = time.Now()
+				reply.Task = task
+				reply.ReducerNum = c.ReducerNum
+				reply.AllDone = false
+				log.Println("Assign map task: ", task)
+				c.Lock.Unlock()
+				return nil
+			}
+		}
+
+		c.Lock.Unlock()
+
+		if c.ifAllMapDone(){
+			c.genReduceTasks()
+			c.Lock.Lock()
+			c.AssighPhase = ReducePhase
+			c.Lock.Unlock()
+			
+			err := c.rmMapTmpFiles()
+			if err != nil{
+				log.Println("AssignTask: remove map tmp files error: ", err)
+			}
+		}
+		log.Pringln("No map task to assign")
+		return errors.New("No map task to assign")
+	}else{ //c.AssignPhase == ReducePhase
+		for i := 0; i < c.ReducerNum; i++{
+			task := <- c.ReduceChannel
+			c.ReduceChannel <- task
+			
+			if task.TaskStatus == Ready{
+				task.ReduceWorkerId = c.ReduceWorkerId
+				c.ReduceWorkerId++
+				task.TaskStatus = Running
+				task.BeginTime = time.Now()
+				reply.Task = task
+				reply.AllDone = false
+				log.Println("Assign reduce task: ", task)
+				c.Lock.Unlock()
+				return nil
+			}
+		}
+
+		c.Lock.Unlock()
+		if c.ifAllReduceDone(){
+			reply.AllDone = true
+			err := c.rmMapFinalFiles()
+			if err != nil{
+				log.Println("AssignTask: remove map final files error: ", err)
+			}
+			err := c.rmReduceTmpFiles()
+			if err != nil{
+				log.Println("AssignTask: remove reduce tmp files error: ", err)
+			}
+			return nil
+		}
+
+		log.Println("No reduce task to assign")
+		return errors.New("No reduce task to assign")
+	}
+}
+
+func (c *Coordinator) MapTaskDone(args *MapTaskDoneArgs, reply *MapTaskDoneReply) error {
+	c.Lock.Lock()
+	if c.AllMapDone {
+	   c.Lock.Unlock()
+	   return errors.New("All map done")
+	}
+ 
+	for i := 0; i < c.MapperNum; i++ {
+	   task := <-c.MapChannel
+	   c.MapChannel <- task
+	   if task.TaskStatus == Running && time.Now().Sub(task.BeginTime) > (TaskExpiredTime)*time.Second {
+		  task.TaskStatus = Ready
+		  log.Printf("Map task with MapWorkerId = %d expired", task.MapWorkerId)
+		  continue
+	   }
+ 
+	   if args.MapWorkerId == task.MapWorkerId && task.TaskStatus == Running && time.Now().Sub(task.BeginTime) <= TaskExpiredTime*time.Second {
+		  task.TaskStatus = Finished
+		  err := c.genMapFinalFiles(args.Files)
+		  if err != nil {
+			 task.TaskStatus = Ready
+			 reply.Err = err
+			 log.Println("MapTaskDone.genMapFinalFiles err = ", err)
+			 c.Lock.Unlock()
+			 return err
+		  }
+		  log.Printf("Map task with MapWorkerId = %d is finished in time", task.MapWorkerId)
+		  c.Lock.Unlock()
+		  return nil
+	   }
+	}
+	c.Lock.Unlock()
+ 
+	reply.Err = errors.New(fmt.Sprintf("Map task with MapWorkerId = %d cannot be done", args.MapWorkerId))
+	log.Println(fmt.Sprintf("Map task with MapWorkerId = %d cannot be done", args.MapWorkerId))
+	return errors.New(fmt.Sprintf("Map task with MapWorkerId = %d cannot be done", args.MapWorkerId))
+ }
+
+func (c *Coordinator) genMapFinalFiles(files []string) error {
+	for _, file := range files {
+	   tmp_file, err := os.Open(file)
+	   if err != nil {
+		  log.Println("genMapFinalFiles err = ", err)
+		  return err
+	   }
+	   defer tmp_file.Close()
+	   tmp_file_name := filepath.Base(file)
+	   final_file_path := FinalMapFilePath + tmp_file_name
+	   final_file, err := os.Create(final_file_path)
+	   if err != nil {
+		  log.Println("genMapFinalFiles.os.Create err = ", err)
+		  return err
+	   }
+	   defer final_file.Close()
+	   _, err = io.Copy(final_file, tmp_file)
+	   if err != nil {
+		  log.Println("genMapFinalFiles.io.Copy err = ", err)
+		  return err
+	   }
+	}
+	return nil
+}
+
+ // Response to reduce task done request from worker
+func (c *Coordinator) ReduceTaskDone(args *ReduceTaskDoneArgs, reply *ReduceTaskDoneReply) error {
+
+	c.Lock.Lock()
+	if c.AllReduceDone {
+	   log.Println("All reduce task done")
+	   c.Lock.Unlock()
+	   return errors.New("All reduce task done")
+	}
+ 
+	for i := 0; i < c.ReducerNum; i++ {
+	   task := <-c.ReduceChannel
+	   c.ReduceChannel <- task
+	   if task.TaskStatus == Running && time.Now().Sub(task.BeginTime) > (TaskExpiredTime)*time.Second {
+		  task.TaskStatus = Ready
+		  log.Printf("Reduce task with ReduceWorkerId = %d expired", task.ReduceWorkerId)
+		  continue
+	   }
+	   if args.ReduceWorkerId == task.ReduceWorkerId && task.TaskStatus == Running && time.Now().Sub(task.BeginTime) <= (TaskExpiredTime)*time.Second {
+		  task.TaskStatus = Finished
+		  err := c.genReduceFinalFile(args.File)
+		  if err != nil {
+			 log.Println("ReduceTaskDone.genReduceFinalFile err = ", err)
+			 task.TaskStatus = Ready
+			 reply.Err = err
+			 c.Lock.Unlock()
+			 return err
+		  }
+		  log.Printf("Reduce task with ReduceWorkerId = %d is finished in time", task.ReduceWorkerId)
+		  c.Lock.Unlock()
+		  return nil
+ 
+	   }
+	}
+	c.Lock.Unlock()
+ 
+	reply.Err = errors.New(fmt.Sprintf("Reduce task with ReduceWorkerId = %d cannot be done", args.ReduceWorkerId))
+	log.Println(fmt.Sprintf("Reduce task with ReduceWorkerId = %d cannot be done", args.ReduceWorkerId))
+	return errors.New(fmt.Sprintf("Reduce task with ReduceWorkerId = %d cannot be done", args.ReduceWorkerId))
+ }
+ 
+ // Generate Reduce task final file
+ func (c *Coordinator) genReduceFinalFile(file string) error {
+	tmp_file, err := os.Open(file)
+	if err != nil {
+	   log.Println("genReduceFinalFile.os.Open err = ", err)
+	   return err
+	}
+	defer tmp_file.Close()
+	tmp_file_name := filepath.Base(file)
+	final_file_path := FinalReduceFilePath + tmp_file_name
+	final_file, err := os.Create(final_file_path)
+	if err != nil {
+	   log.Println("genReduceFinalFile.os.Create err = ", err)
+	   return err
+	}
+	defer final_file.Close()
+	_, err = io.Copy(final_file, tmp_file)
+	if err != nil {
+	   log.Println("genReduceFinalFile.os.Copy err = ", err)
+	   return err
+	}
+	return nil
+ }
+ 
+ // Remove map task's  temporary files
+ func (c *Coordinator) rmMapTmpFiles() error {
+	d, err := os.Open(TmpMapFilePath)
+	if err != nil {
+	   log.Println("rmMapTmpFiles os.Open err = ", err)
+	   return err
+	}
+	defer d.Close()
+	names, err := d.Readdirnames(-1)
+	if err != nil {
+	   log.Println("rmMapTmpFiles.d.Readdirnames err = ", err)
+	   return err
+	}
+	for _, name := range names {
+	   err = os.RemoveAll(TmpMapFilePath + name)
+	   if err != nil {
+		  log.Println("rmMapTmpFiles.os.RemoveAll err = ", err)
+		  return err
+	   }
+	}
+	return nil
+ }
+ 
+ // Remove map task's final files
+ func (c *Coordinator) rmMapFinalFiles() error {
+	d, err := os.Open(FinalMapFilePath)
+	if err != nil {
+	   log.Println("rmMapFinalFiles.os.Open err = ", err)
+	   return err
+	}
+	defer d.Close()
+	names, err := d.Readdirnames(-1)
+	if err != nil {
+	   log.Println("rmMapFinalFiles.d.Readdirnames err = ", err)
+	   return err
+	}
+	for _, name := range names {
+	   err = os.RemoveAll(FinalMapFilePath + name)
+	   if err != nil {
+		  log.Println("rmMapFinalFiles.os.RemoveAll err = ", err)
+		  return err
+	   }
+	}
+	return nil
+ }
+ 
+// Remove reduce task's temporary files
+func (c *Coordinator) rmReduceTmpFiles() error {
+	d, err := os.Open(TmpReduceFilePath)
+	if err != nil {
+	   log.Println("rmReduceTmpFiles.os.Open err = ", err)
+	   return err
+	}
+	defer d.Close()
+	names, err := d.Readdirnames(-1)
+	if err != nil {
+	   log.Println("rmReduceTmpFiles.d.Readdirnames err = ", err)
+	   return err
+	}
+	for _, name := range names {
+	   err = os.RemoveAll(TmpReduceFilePath + name)
+	   if err != nil {
+		  log.Println("rmReduceTmpFiles.os.RemoveAll err = ", err)
+		  return err
+	   }
+	}
+	return nil
+}
+ 
+// Vist every task in map channel and find out if all map task were done
+func (c *Coordinator) ifAllMapDone() bool {
+	c.Lock.Lock()
+	for i := 0; i < c.MapperNum; i++ {
+	   task := <-c.MapChannel
+	   if task.TaskStatus != Finished {
+		  c.MapChannel <- task
+		  c.Lock.Unlock()
+		  return false
+	   }
+	   c.MapChannel <- task
+	}
+	c.AllMapDone = true
+	c.Lock.Unlock()
+	return true
+}
+ 
+// Vist every task in reduce channel and find out if all reduce task were done
+func (c *Coordinator) ifAllReduceDone() bool {
+	c.Lock.Lock()
+	for i := 0; i < c.ReducerNum; i++ {
+	   task := <-c.ReduceChannel
+	   c.ReduceChannel <- task
+	   if task.TaskStatus != Finished {
+		  c.Lock.Unlock()
+		  return false
+	   }
+	}
+	c.AllReduceDone = true
+	c.Lock.Unlock()
+	return true
+}
+ 
 
 //
 // start a thread that listens for RPCs from worker.go
@@ -110,9 +448,13 @@ func (c *Coordinator) genReduceTasks(){
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
+
+
 	//l, e := net.Listen("tcp", ":1234")
-	sockname := coordinatorSock()
+	sockname := "/var/tmp/824-mr-"
+	sockname += strconv.Itoa(os.Getuid())
 	os.Remove(sockname)
+	
 	l, e := net.Listen("unix", sockname)
 	if e != nil {
 		log.Fatal("listen error:", e)
@@ -125,10 +467,13 @@ func (c *Coordinator) server() {
 // if the entire job has finished.
 //
 func (c *Coordinator) Done() bool {
-	ret := false
-
-	// Your code here.
-
-
-	return ret
+	c.Lock.Lock()
+	if c.AllMapDone && c.AllReduceDone{
+		c.Lock.Unlock()
+		log.Println("All tasks done!")
+		time.Sleep(3 * time.Second) //Wait for worker to exit the program before the main end
+		return true
+	}
+	c.Lock.Unlock()
+	return false
 }
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,110 +1,358 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+   "encoding/json"
+   "fmt"
+   "hash/fnv"
+   "io"
+   "log"
+   "net/rpc"
+   "os"
+   "path/filepath"
+   "sort"
+   "strconv"
+)
 
-
-//
-// Map functions return a slice of KeyValue.
-//
-type KeyValue struct {
-	Key   string
-	Value string
+type MRWorker struct {
+   WorkerId   int                             // WorkerId, initialized by task.MapWorkerId or task.ReduceId
+   MapFunc    func(string, string) []KeyValue // Map function, initialized by main function
+   ReduceFunc func(string, []string) string   // Reduce function, initialized by main function
+   Task       *Task                           // Task, given by coordinator.
+   NReduce    int                             // the numer of reduce worker, given by coordinator
+   IsDone     bool                            // the variable indicates if all task were done
 }
 
-//
-// use ihash(key) % NReduce to choose the reduce
-// task number for each KeyValue emitted by Map.
-//
+// Initialized the worker
+func Worker(map_func func(string, string) []KeyValue, reduce_func func(string, []string) string) {
+   worker := MRWorker{
+      WorkerId:   -1, // initialized to -1 when the worker isn't handle a task
+      MapFunc:    map_func,
+      ReduceFunc: reduce_func,
+      IsDone:     false,
+   }
+   log.Println("Initial worker done")
+   worker.work()
+   log.Println("Worker with WorkerId = ", worker.WorkerId, " finished all works")
+}
+
+// Loop working as long as all tasks wern't finished
+func (worker *MRWorker) work() {
+   for !worker.IsDone {
+      task, err := worker.reqTask()
+      if err != nil {
+         log.Println("work.reqTask err = ", err)
+         continue
+      }
+
+      if task == nil {
+         log.Printf("Worker with WorkerId = %d received all tasks done", worker.WorkerId)
+         worker.IsDone = true
+         break
+      }
+
+      log.Printf("Worker with WorkerId = %d received task = %v", worker.WorkerId, task)
+      worker.Task = task
+
+      if task.TaskType == Map {
+         worker.WorkerId = task.MapWorkerId
+         err := worker.doMap()
+         if err != nil {
+            log.Println("worker.doMap err = ", err)
+            time.Sleep(time.Second)
+            continue
+         }
+         log.Println("Map task done, task = ", task)
+      } else {
+         worker.WorkerId = task.ReduceWorkerId
+         err := worker.doReduce()
+         if err != nil {
+            log.Println("worker.doReduce err = ", err)
+            continue
+         }
+         log.Println("Reduce task done, task = ", task)
+      }
+   }
+}
+
+// Request task to coordinator
+func (worker *MRWorker) reqTask() (*Task, error) {
+   args := TaskArgs{}
+   reply := TaskReply{}
+   err := call("Coordinator.AssignTask", &args, &reply)
+   if err != nil {
+      log.Println("reqTask.call err = ", err)
+      return nil, err
+   }
+   worker.Task = reply.Task
+   if reply.AllDone {
+      worker.IsDone = true
+      return nil, nil
+   }
+   worker.NReduce = reply.ReducerNum
+   return reply.Task, nil
+}
+
+// Execute map tesk details
+func (worker *MRWorker) doMap() error {
+   task := worker.Task
+
+   intermediate, err := worker.genIntermediate(task.InputFile)
+   if err != nil {
+      log.Println("doMap.genIntermediate err = ", err)
+      return err
+   }
+
+   tmp_files, err := worker.writeIntermediateToTmpFiles(intermediate)
+   if err != nil {
+      log.Println("doMap.writeIntermediateToTmpFiles :", err)
+      return err
+   }
+
+   err = worker.mapTaskDone(tmp_files)
+   if err != nil {
+      log.Println("doMap.mapTaskDone err = ", err)
+      for _, file := range tmp_files {
+         err := os.Remove(file)
+         if err != nil {
+            log.Println("worker.mapTaskDone.os.Remove err = ", err)
+         }
+      }
+      return err
+   }
+   return nil
+}
+
+// Generate intermediate key-val list
+func (worker *MRWorker) genIntermediate(filename string) ([]KeyValue, error) {
+   intermediate := make([]KeyValue, 0)
+   file, err := os.Open(filename)
+   if err != nil {
+      log.Println("genIntermediate.os.Open", err)
+      return nil, err
+   }
+   content, err := io.ReadAll(file)
+   if err != nil {
+      log.Println("genIntermediate.io.ReadAll", err)
+      return nil, err
+   }
+   defer file.Close()
+
+   kva := worker.MapFunc(filename, string(content))
+   intermediate = append(intermediate, kva...)
+   return intermediate, nil
+}
+
+// Write intermediate to map task's temporary files
+func (worker *MRWorker) writeIntermediateToTmpFiles(intermediate []KeyValue) ([]string, error) {
+
+   tmp_files := []string{}
+   hashed_intermediate := make([][]KeyValue, worker.NReduce)
+
+   for _, kv := range intermediate {
+      hash_val := ihash(kv.Key) % worker.NReduce
+      hashed_intermediate[hash_val] = append(hashed_intermediate[hash_val], kv)
+   }
+
+   for i := 0; i < worker.NReduce; i++ {
+      tmp_file, err := os.CreateTemp(TmpMapFilePath, "mr-*.txt")
+      if err != nil {
+         log.Println("writeIntermediateToTmpFiles.os.CreateTemp err = ", err)
+         return nil, err
+      }
+      defer os.Remove(tmp_file.Name())
+      defer tmp_file.Close()
+
+      enc := json.NewEncoder(tmp_file)
+      for _, kv := range hashed_intermediate[i] {
+         err := enc.Encode(&kv)
+         if err != nil {
+            log.Println("writeIntermediateToTmpFiles.enc.Encode", err)
+            return nil, err
+         }
+      }
+
+      file_path := fmt.Sprintf("mr-%v-%v", worker.WorkerId, i)
+      err = os.Rename(tmp_file.Name(), TmpMapFilePath+file_path)
+      if err != nil {
+         log.Println("writeIntermediateToTmpFiles os.Rename: ", err)
+         return nil, err
+      }
+      tmp_files = append(tmp_files, TmpMapFilePath+file_path)
+   }
+
+   return tmp_files, nil
+}
+
+// Report map task done to coordinator
+func (worker *MRWorker) mapTaskDone(files []string) error {
+   args := MapTaskDoneArgs{
+      MapWorkerId: worker.WorkerId,
+      Files:       files,
+   }
+   reply := MapTaskDoneReply{}
+   err := call("Coordinator.MapTaskDone", &args, &reply)
+   if err != nil {
+      log.Println("mapTaskDone.call err = ", err)
+      return err
+   }
+   if reply.Err != nil {
+      log.Println("mapTaskDone.reply.Err = ", reply.Err)
+      return reply.Err
+   }
+   return nil
+}
+
+// Execute reduce tesk details
+func (worker *MRWorker) doReduce() error {
+
+   intermediate, err := worker.collectIntermediate(worker.Task.InputFile)
+   if err != nil {
+      log.Println("doReduce.collectIntermediate err = ", err)
+      return err
+   }
+
+   sort.Sort(ByKey(intermediate))
+
+   res := worker.genReduceRes(intermediate)
+
+   tmp_file, err := worker.writeReduceResToTmpFile(res)
+   if err != nil {
+      log.Println("doReduce.writeReduceResToTmpFile err = ", err)
+      return err
+   }
+
+   err = worker.reduceTaskDone(tmp_file)
+   if err != nil {
+      log.Println("doReduce.reduceTaskDone err = ", err)
+      err := os.Remove(tmp_file)
+      if err != nil {
+         log.Println("doReduce.os.Remove err = ", err)
+      }
+      return err
+   }
+
+   return nil
+}
+
+// Collect intermediate from different map workers' result files
+func (worker *MRWorker) collectIntermediate(file_pattern string) ([]KeyValue, error) {
+   intermediate := make([]KeyValue, 0)
+   files, err := filepath.Glob(file_pattern)
+   if err != nil {
+      log.Println("collectIntermediate.filepath.Glob err = ", err)
+      return nil, err
+   }
+
+   for _, file_path := range files {
+      file, err := os.Open(file_path)
+      if err != nil {
+         log.Println("collectIntermediateos.Open err = ", err)
+         return nil, err
+      }
+      dec := json.NewDecoder(file)
+      for {
+         var kv KeyValue
+         if err := dec.Decode(&kv); err != nil {
+            break
+         }
+         intermediate = append(intermediate, kv)
+      }
+   }
+
+   return intermediate, nil
+}
+
+// Gen reduce result
+func (worker *MRWorker) genReduceRes(intermediate []KeyValue) []KeyValue {
+   res := make([]KeyValue, 0)
+   i := 0
+   for i < len(intermediate) {
+      //  the key in intermediate [i...j]  is the same since intermediate is already sorted
+      j := i + 1
+      for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+         j++
+      }
+
+      // sum the val number of intermediate [i...j]
+      values := []string{}
+      for k := i; k < j; k++ {
+         values = append(values, intermediate[k].Value)
+      }
+      output := worker.ReduceFunc(intermediate[i].Key, values)
+
+      kv := KeyValue{Key: intermediate[i].Key, Value: output}
+      res = append(res, kv)
+
+      i = j
+   }
+   return res
+}
+
+// write reduce task's result to temporary file
+func (worker *MRWorker) writeReduceResToTmpFile(res []KeyValue) (string, error) {
+   tempFile, err := os.CreateTemp(TmpReduceFilePath, "mr-") ///home/distributed_system/tmp_res_file/mr-xxxxx(随机字符串)
+   if err != nil {
+      log.Println("writeReduceResToTmpFile.os.CreateTemp err = ", err)
+      return "", err
+   }
+
+   // write key-val pair into tmp file
+   for _, kv := range res {
+      fmt.Fprintf(tempFile, "%s %s\n", kv.Key, kv.Value)
+   }
+
+   temp_name := TmpReduceFilePath + "mr-out-" + strconv.Itoa(worker.WorkerId) + ".txt"
+   err = os.Rename(tempFile.Name(), temp_name)
+   if err != nil {
+      log.Println("writeReduceResToTmpFile.os.Rename err = ", err)
+      return "", err
+   }
+
+   return temp_name, nil
+}
+
+// Report reduce task done to coordinator
+func (worker *MRWorker) reduceTaskDone(file string) error {
+   args := ReduceTaskDoneArgs{
+      ReduceWorkerId: worker.WorkerId,
+      File:           file,
+   }
+   reply := ReduceTaskDoneReply{}
+   err := call("Coordinator.ReduceTaskDone", &args, &reply)
+   if err != nil {
+      log.Println("reduceTaskDone.call ", err)
+      return err
+   }
+   if reply.Err != nil {
+      log.Println(err)
+      return reply.Err
+   }
+   return nil
+}
+
 func ihash(key string) int {
-	h := fnv.New32a()
-	h.Write([]byte(key))
-	return int(h.Sum32() & 0x7fffffff)
+   h := fnv.New32a()
+   h.Write([]byte(key))
+   return int(h.Sum32() & 0x7fffffff)
 }
 
+// rpc call function that send request to coordinator
+func call(fun_name string, args interface{}, reply interface{}) error {
 
-//
-// main/mrworker.go calls this function.
-//
-func Worker(mapf func(string, string) []KeyValue,
-	reducef func(string, []string) string) {
+   sockname := "/var/tmp/824-mr-"
+   sockname += strconv.Itoa(os.Getuid())
+   c, err := rpc.DialHTTP("unix", sockname)
+   if err != nil {
+      log.Fatal("dialing:", err)
+      return err
+   }
+   defer c.Close()
 
-	// Your worker implementation here.
-	
-	for{
-		response := doHeartbeat()
-		log.Printf("Worker: recive corordinators's heartbeat: %v\n", response)
-		
-		switch response.JobType{{
-		case MapJob:
-			doMapJob(response, mapf)
-		case ReduceJob:
-			doReduceJob(reduceF, response)
-		case WaitJob:
-			time.Sleep(1 * time.Second)
-		case CompleteJob:
-			return
-		default:
-			panic(fmt.sprintf("Unknow job type: %v", response.JobType))
-		}
-	}
+   err = c.Call(fun_name, args, reply)
+   if err != nil {
+      log.Println("call.call", err)
+      return err
+   }
 
-
-	// uncomment to send the Example RPC to the coordinator.
-	//CallExample()
-
-}
-
-//
-// example function to show how to make an RPC call to the coordinator.
-//
-// the RPC argument and reply types are defined in rpc.go.
-//
-func CallExample() {
-
-	// declare an argument structure.
-	args := ExampleArgs{}
-
-	// fill in the argument(s).
-	args.X = 99
-
-	// declare a reply structure.
-	reply := ExampleReply{}
-
-	// send the RPC request, wait for the reply.
-	// the "Coordinator.Example" tells the
-	// receiving server that we'd like to call
-	// the Example() method of struct Coordinator.
-	ok := call("Coordinator.Example", &args, &reply)
-	if ok {
-		// reply.Y should be 100.
-		fmt.Printf("reply.Y %v\n", reply.Y)
-	} else {
-		fmt.Printf("call failed!\n")
-	}
-}
-
-//
-// send an RPC request to the coordinator, wait for the response.
-// usually returns true.
-// returns false if something goes wrong.
-//
-func call(rpcname string, args interface{}, reply interface{}) bool {
-	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
-	sockname := coordinatorSock()
-	c, err := rpc.DialHTTP("unix", sockname)
-	if err != nil {
-		log.Fatal("dialing:", err)
-	}
-	defer c.Close()
-
-	err = c.Call(rpcname, args, reply)
-	if err == nil {
-		return true
-	}
-
-	fmt.Println(err)
-	return false
+   return nil
 }
